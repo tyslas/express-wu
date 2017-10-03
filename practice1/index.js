a6a770668bafca631f6325c548cb2fd6aedefdec
Tito Yslas <roberto.yslas@gmail.com>
2017-10-03 09:58:58 -0600

    practice setup

---
+++ practice1/index.js
@@ -0,0 +1,10 @@
+var express = require('express');
+var app = express();
+
+app.listen(3000, function() {
+  console.log("hi practice1");
+});
+
+app.get('/', function(req, res) {
+  res.send('hiiiiiii! (1)')
+});
